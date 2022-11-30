eee247399a5852574a2b8602fcdb2353c5cb97e1
MRubioMunoz <miguelrubiomunoz@gmail.com>
2022-11-30 12:19:48 +0100

    Test Update oportunity

--- src/test/java/com/solera/crm/SoleraCRM/SoleraCrmApplicationTests.java
+++ src/test/java/com/solera/crm/SoleraCRM/SoleraCrmApplicationTests.java
@@ -1,13 +1,27 @@
 package com.solera.crm.SoleraCRM;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.solera.crm.SoleraCRM.models.Oportunity;
+import com.solera.crm.SoleraCRM.repo.ContactDao;
+import com.solera.crm.SoleraCRM.repo.OportunityDao;
 
 @SpringBootTest
 class SoleraCrmApplicationTests {
+	
+	@Autowired
+	OportunityDao oportunityDao;
 
 	@Test
-	void contextLoads() {
+	void updateOportunityTest() {
+		Oportunity oUpdate = new Oportunity((long) 1, "UpdateTest","666333555", "test@solera.com", false, ContactDao.contacts);
+		oportunityDao.updateOportunity(oUpdate);
+		assertEquals(oportunityDao.findOne((long) 1).getName(), "UpdateTest");
+		assertEquals(oportunityDao.findOne((long) 1).getEmail(), "test@solera.com");
 	}
 
 }
